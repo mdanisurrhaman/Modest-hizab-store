dec79643cf95ff12e827913761395eb3e78bccfb
Md Anisurrhaman <166200118+mdanisurrhaman@users.noreply.github.com>
2025-02-22 19:10:23 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from MHS_app.models import  *
+# Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Cart)
+admin.site.register(Cartitem)
+admin.site.register(Employee)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Product_variation)
+admin.site.register(Variation_option)
+admin.site.register(Variation)
+admin.site.register(Quotation)
+admin.site.register(Collections)
+admin.site.register(Most_Popular)
+admin.site.register(Address)
+admin.site.register(Wishlist)
+admin.site.register(Order)
+admin.site.register(Order_Status)
+admin.site.register(Payment)
+admin.site.register(Review)
+
